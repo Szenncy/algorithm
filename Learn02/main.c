c9f353a65110163a45a3d1c322a08f4e5c7f9a91
证翕 胡 <zhengxihu@zhengxideMacBook-Pro.local>
2015-07-16 21:43:18 +0800

    Initial Commit

---
+++ Learn02/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Learn02
+//
+//  Created by 证翕 胡 on 15/7/16.
+//  Copyright (c) 2015年 证翕 胡. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
